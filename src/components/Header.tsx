54b9c2470fbc6ca1219863355ad28969c35053f3
surya13131 <suryasurya20092005@gmail.com>
2026-01-28 12:04:52 +0530

    Add project updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
+
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import logoImg from '../assets/workman LOGO.png';
