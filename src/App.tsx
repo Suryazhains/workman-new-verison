8f7a5b8ed6c2eefc519806ca81cca83b2b0216d3
Suryazhains <surya@zhians.com>
2026-03-19 21:20:31 +0530

    first commit and fixed and fix and

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+// import React, { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './components/Landingpage';
